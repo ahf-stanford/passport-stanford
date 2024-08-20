18529ceda2cffdf68a35b5359ac1a380dff9ee76
Arash Hooshyari Far <ahf@stanford.edu>
2024-08-20 15:25:37 -0700

    Library and Test upgraded to support Node 20

--- index.js
+++ index.js
@@ -1,3 +1,111 @@
+const saml = require('passport-saml')
+const fs = require('fs')
+const util = require('util')
+const url = require('url')
+const idps = require('./lib/idps')
+const attrmap = require('./lib/attributes')
+
+// An extension of the Passport SAML strategy for Stanford.
+class Strategy extends saml.Strategy {
+  constructor(options, verify) {
+    // some sensible defaults
+    options.protocol = options.protocol || 'https://'
+    options.signatureAlgorithm = options.signatureAlgorithm || 'sha256'
+    options.identifierFormat = options.identifierFormat || 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient'
+    options.acceptedClockSkewMs = options.acceptedClockSkewMs || 60000
+    options.attributeConsumingServiceIndex = options.attributeConsumingServiceIndex || false
+    options.forceAuthn = options.forceAuthn || false
+    options.skipRequestCompression = options.skipRequestCompression || false
+
+    if (options.disableRequestedAuthnContext === undefined) {
+      options.disableRequestedAuthnContext = true
+    }
+
+    if (options.validatedInResponseTo === undefined) {
+      options.validateInResponseTo = true
+    }
+
+    if (options.decryptionCertPath) {
+      options.decryptionCert = fs.readFileSync(options.decryptionCertPath, 'utf8')
+    }
+
+    if (options.decryptionPvkPath) {
+      options.decryptionPvk = fs.readFileSync(options.decryptionPvkPath, 'utf8')
+    }
+
+    if (options.entityID) {
+      options.issuer = options.entityID
+    }
+
+    if (options.entityId) {
+      options.issuer = options.entityId
+    }
+
+    if (options.idp) {
+      if (idps[options.idp]) {
+        options.entryPoint = idps[options.idp].entryPoint
+        options.cert = idps[options.idp].cert
+      } else {
+        throw new Error('Unknown IdP: ' + options.idp)
+      }
+    }
+
+    if (!options.entryPoint || !options.cert) {
+      console.warn('No IdP defined - defaulting to ' + idps.dev.entityID)
+      options.entryPoint = idps.dev.entryPoint
+      options.cert = idps.dev.cert
+    }
+
+    if (!options.issuer) {
+      throw new Error('No entityId defined!')
+    }
+
+    // having either both an encryption cert and private key is valid
+    // having neither an encryption cert nor a private key is also valid
+    // having only one or the other is NOT valid
+    if (!options.decryptionCert && options.decryptionPvk) {
+      throw new Error('Only a private key was defined; a public cert is also required')
+    }
+
+    if (options.decryptionCert && !options.decryptionPvk) {
+      throw new Error('Only a public cert was defined; a private key is also required')
+    }
+
+    if (!options.loginPath) {
+      throw new Error('No loginPath defined!')
+    }
+
+    super(options, (req, profile, done) => {
+      req.session.strategy = this.name
+      this.attributeMapper(profile, done)
+    })
+
+    this.loginPath = options.loginPath
+
+    // set up an attribute mapper
+    this.attributeMapper = attrmap(options.attributeMap)
+
+    // set the name of this strategy to either the name passed in
+    // via the options, or the short name of the idp.
+    //
+    // if neither is set, the name will be 'suSAML'
+    this.name = options.name || options.idp || 'suSAML'
+  }
+
+  protect() {
+    return (req, res, next) => {
+      if (req.isAuthenticated() && req.session.strategy === this.name) {
+        return next()
+      } else {
+        // Handle unauthenticated access
+      }
+    }
+  }
+}
+
+module.exports = Strategy
+
+/*
 var saml    = require('passport-saml'),
     fs      = require('fs'),
     util    = require('util'),
@@ -5,9 +113,9 @@ var saml    = require('passport-saml'),
     idps    = require('./lib/idps'),
     attrmap = require('./lib/attributes');
 
-/*
-  An extensnion of the Passport SAML strategy for Stanford.
-*/
+
+//  An extensnion of the Passport SAML strategy for Stanford.
+
 function strategy (options, verify) {
 
   // some sensible defaults
@@ -134,3 +242,4 @@ strategy.prototype.metadata = function metadata () {
 };
 
 module.exports.Strategy = strategy;
+*/
--- test/app.js
+++ test/app.js
@@ -1,3 +1,122 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import passport from 'passport'
+import suSAML from 'passport-stanford'
+import path from 'path'
+import http from 'http'
+
+const app = express()
+const samlPath = '/saml'
+let forcedSaml, saml
+
+app.set('port', 3000)
+app.set('views', path.join(process.cwd(), '/app/views'))
+app.set('view engine', 'pug')
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(session({
+  secret: 'sooperS3CRET!',
+  resave: false,
+  saveUninitialized: true,
+  name: 'stanford',
+  cookie: {
+    httpOnly: true,
+    maxAge: 600000
+  }
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+saml = new suSAML.Strategy({
+  name: 'saml',
+  protocol: 'http://',
+  idp: 'stanford',
+  entityId: 'https://github.com/scottylogan/passport-stanford',
+  path: samlPath,
+  loginPath: samlPath,
+  passReqToCallback: true,
+  passport: passport,
+  decryptionPvkPath: './private.pem',
+  decryptionCertPath: './public.pem',
+})
+
+forcedSaml = new suSAML.Strategy({
+  name: 'forced',
+  protocol: 'http://',
+  idp: 'stanford',
+  entityId: 'https://github.com/scottylogan/passport-stanford',
+  path: samlPath,
+  loginPath: samlPath,
+  passReqToCallback: true,
+  passport: passport,
+  forceAuthn: true,
+  decryptionPvkPath: './private.pem',
+  decryptionCertPath: './public.pem',
+})
+
+// Corrected passport.use calls
+passport.use('saml', saml)
+passport.use('forced', forcedSaml)
+
+passport.serializeUser((user, done) => {
+  done(null, JSON.stringify(user))
+})
+
+passport.deserializeUser((json, done) => {
+  try {
+    done(null, JSON.parse(json))
+  } catch (err) {
+    done(err, null)
+  }
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('500', { error: err })
+})
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    user: req.isAuthenticated() ? req.user : null,
+    loginPath: samlPath,
+  })
+})
+
+app.all(samlPath, (req, res, next) => {
+  if (['GET', 'POST'].indexOf(req.method) === -1) {
+    return res.status(405).send('Method not supported')
+  }
+  return passport.authenticate(req.session.strategy, {
+    successReturnToOrRedirect: '/'
+  })(req, res, next)
+})
+
+// Removed the call to saml.metadata() as it is not a function
+app.get('/metadata', (req, res) => {
+  res.status(404).send('Metadata not available')
+})
+
+// Use passport.authenticate to protect the profile route
+app.get('/profile', passport.authenticate('saml', { session: false }), (req, res) => {
+  res.render('profile', {
+    user: req.user,
+  })
+})
+
+const server = http.createServer(app)
+server.listen(app.get('port'), () => {
+  console.log(`Express server listening on port ${app.get('port')}`)
+})
+
+
+
+// Original code:
+/*
 var http          = require('http'),
     path          = require('path'),
     express       = require('express'),
@@ -143,3 +262,4 @@ app.use(function (err, req, res, next) {
 http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
 });
+*/
