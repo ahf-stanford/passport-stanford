daa619567ad5d3555c944ed13f497c6ecc799b3b
Arash Hooshyari Far <ahf@stanford.edu>
2024-08-23 16:01:56 -0700

    a few adjustments

--- test/app.js
+++ test/app.js
@@ -12,7 +12,10 @@ const app = express()
 const samlPath = '/saml'
 let forcedSaml, saml
 
-app.set('port', 3000)
+const PORT = 3000
+const HOST = 'localhost'
+
+app.set('port', PORT)
 app.set('views', path.join(process.cwd(), '/app/views'))
 app.set('view engine', 'pug')
 
@@ -20,43 +23,45 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(session({
-  secret: 'sooperS3CRET!',
-  resave: false,
-  saveUninitialized: true,
-  name: 'stanford',
-  cookie: {
-    httpOnly: true,
-    maxAge: 600000
-  }
+    secret: 'sooperS3CRET!',
+    resave: false,
+    saveUninitialized: true,
+    name: 'stanford',
+    cookie: {
+        httpOnly: true,
+        maxAge: 600000
+    }
 }))
 app.use(passport.initialize())
 app.use(passport.session())
 
 saml = new suSAML.Strategy({
-  name: 'saml',
-  protocol: 'http://',
-  idp: 'stanford',
-  entityId: 'https://github.com/scottylogan/passport-stanford',
-  path: samlPath,
-  loginPath: samlPath,
-  passReqToCallback: true,
-  passport: passport,
-  decryptionPvkPath: './private.pem',
-  decryptionCertPath: './public.pem',
+    name: 'saml',
+    protocol: 'http://',
+    idp: 'stanford',
+    entityId: 'https://github.com/scottylogan/passport-stanford',
+    callbackUrl: `https://${HOST}:${PORT}${samlPath}`,
+    path: samlPath,
+    loginPath: samlPath,
+    passReqToCallback: true,
+    passport: passport,
+    decryptionPvkPath: './private.pem',
+    decryptionCertPath: './public.pem',
 })
 
 forcedSaml = new suSAML.Strategy({
-  name: 'forced',
-  protocol: 'http://',
-  idp: 'stanford',
-  entityId: 'https://github.com/scottylogan/passport-stanford',
-  path: samlPath,
-  loginPath: samlPath,
-  passReqToCallback: true,
-  passport: passport,
-  forceAuthn: true,
-  decryptionPvkPath: './private.pem',
-  decryptionCertPath: './public.pem',
+    name: 'forced',
+    protocol: 'http://',
+    idp: 'stanford',
+    entityId: 'https://github.com/scottylogan/passport-stanford',
+    callbackUrl: `https://${HOST}:${PORT}${samlPath}`,
+    path: samlPath,
+    loginPath: samlPath,
+    passReqToCallback: true,
+    passport: passport,
+    forceAuthn: true,
+    decryptionPvkPath: './private.pem',
+    decryptionCertPath: './public.pem',
 })
 
 // Corrected passport.use calls
@@ -64,52 +69,53 @@ passport.use('saml', saml)
 passport.use('forced', forcedSaml)
 
 passport.serializeUser((user, done) => {
-  done(null, JSON.stringify(user))
+    done(null, JSON.stringify(user))
 })
 
 passport.deserializeUser((json, done) => {
-  try {
-    done(null, JSON.parse(json))
-  } catch (err) {
-    done(err, null)
-  }
+    try {
+        done(null, JSON.parse(json))
+    } catch (err) {
+        done(err, null)
+    }
 })
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500)
-  res.render('500', { error: err })
+    res.status(err.status || 500)
+    res.render('500', { error: err })
 })
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    user: req.isAuthenticated() ? req.user : null,
-    loginPath: samlPath,
-  })
+    res.render('home', {
+        user: req.isAuthenticated() ? req.user : null,
+        loginPath: samlPath,
+    })
 })
 
 app.all(samlPath, (req, res, next) => {
-  if (['GET', 'POST'].indexOf(req.method) === -1) {
-    return res.status(405).send('Method not supported')
-  }
-  return passport.authenticate(req.session.strategy, {
-    successReturnToOrRedirect: '/'
-  })(req, res, next)
+    if (['GET', 'POST'].indexOf(req.method) === -1) {
+        return res.status(405).send('Method not supported')
+    }
+    return passport.authenticate(req.session.strategy, {
+        successReturnToOrRedirect: '/'
+    })(req, res, next)
 })
 
 // Removed the call to saml.metadata() as it is not a function
 app.get('/metadata', (req, res) => {
-  res.status(404).send('Metadata not available')
+    //saml.strategy.metadata()
+    res.status(404).send('Metadata not available')
 })
 
 // Use passport.authenticate to protect the profile route
 app.get('/profile', passport.authenticate('saml', { session: false }), (req, res) => {
-  res.render('profile', {
-    user: req.user,
-  })
+    res.render('profile', {
+        user: req.user,
+    })
 })
 
 const server = http.createServer(app)
 server.listen(app.get('port'), () => {
-  console.log(`Express server listening on port ${app.get('port')}`)
+    console.log(`Express server listening on port ${app.get('port')}`)
 })
 
